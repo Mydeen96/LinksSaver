871ffdf0018b59a3ccf8b443c6f39d076246574a
Mydeen <Mydeen1193@gmail.com>
2024-08-03 21:07:07 +0530

    Completed
    Add some function and style ad the chrome extension  is completed

--- script.js
+++ script.js
@@ -2,33 +2,50 @@ let myleads = []
 const inputEl = document.getElementById("input-box");
 const saveEl = document.getElementById("save-btn") ;
 const ulEl = document.getElementById("ul-el");
-const para = document.getElementById("para");
+const tabBtn = document.getElementById("tab-btn");
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myleads") );
 
-
-for(i=0 ;i <= 100 ;i++ ){
-    para.innerHTML += "love you pondati 💑" + "<br>" ;
+if(leadsFromLocalStorage){
+    myleads = leadsFromLocalStorage ;
+    render(myleads)
 }
 
-localStorage.setItem("myleads" , "www.google.com")
-
 saveEl.addEventListener('click',function(){
-        if(inputEl.value !== ""){
+        if(inputEl.value !== "" ){
+            inputEl.placeholder = ""
             myleads.push(inputEl.value);
-        inputEl.value = "";
-        console.log(localStorage.getItem("myleads"))
-        renderLead()
+            inputEl.value = "";
+            render(myleads)
+            localStorage.setItem("myleads" , JSON.stringify(myleads))
         }
         else{
-            alert('please eneter the input')
+            inputEl.placeholder = "Please type the link"
         }
 })
 
-function renderLead(){
+tabBtn.addEventListener('click' , function(){
+    inputEl.placeholder = ""
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){    
+        myleads.push(tabs[0].url)
+        localStorage.setItem("myleads" , JSON.stringify(myleads) )
+        render(myleads)
+    })
+})
+
+deleteBtn.addEventListener('dblclick', function(){
+    inputEl.placeholder = ""
+    localStorage.clear()
+    myleads = []
+    render(myleads)
+})
+
+function render(leads){
     let listItems = "";
-    for (let i = 0 ; i < myleads.length ; i++){
+    for (let i = 0 ; i < leads.length ; i++){
         listItems += `
         <li>
-            <a href='${myleads[i]}' target='_blank'>${myleads[i]}</a>
+            <a href='${leads[i]}' target='_blank'>${leads[i]}</a>
         </li>
         `
     }
